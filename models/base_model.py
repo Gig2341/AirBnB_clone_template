2a6b674d82a0d87ccc2944c15e748a5fe35b3bf0
Gig2341 <danmata2341@gmail.com>
2023-02-16 19:10:36 +0000

    base model done

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,48 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+import models
+
+'''this module represents the base model'''
+
+class BaseModel():
+	'''
+		This class represents the base model
+	'''
+	def __init__(self, id, created_at, updated_at):
+		"""
+		Initializes the BaseModel class
+		
+		Args:
+			id : unique string id of each instance
+			created_at : current datetime when an instance is created
+			updated_at :  current datetime when an instance is updated
+		"""
+		
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+
+
+	def __str__(self):
+		"""
+			string representaion of the class
+		"""
+		return [__class__.__name__] (self.id) self.__dict__
+
+	def save(self):
+		"""
+			updates the public instance attribute updated_at with the current datetime
+		"""
+		self.updated_at = datetime.now()
+
+	def to_dict(self):
+		"""
+			returns a dictionary containing all keys/values of __dict__ of the instance
+		"""
+		my_dict = self.__dict__
+		my_dict["__class__"] = type(self).__name__
+		my_dict["created_at"] = self.created_at.isoformat()
+		my_dict["updated_at"] = self.updated_at.isoformat()
+		return my_dict
 
