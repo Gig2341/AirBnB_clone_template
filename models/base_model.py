d5bd10d9a5e5e9635608c16a619d22bca7d3f8e5
Gig2341 <danmata2341@gmail.com>
2023-02-17 21:15:44 +0000

    task 2 done

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,14 @@ class BaseModel:
 			updated_at :  current datetime when an instance is updated
 		"""
 		
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+		if len(kwargs) != 0:
+			self.__dict__ = kwargs
+			self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+			self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
 
 	def __str__(self):
