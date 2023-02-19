3b3c5f19d485a757cfaebf7846377e811faf5b87
Gig2341 <danmata2341@gmail.com>
2023-02-19 16:25:32 +0000

    task 5 done

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from models.engine.file.storage import FileStorage
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 
 '''this module represents the base model'''
@@ -27,6 +28,7 @@ class BaseModel:
 			self.id = str(uuid.uuid4())
 			self.created_at = datetime.now()
 			self.updated_at = datetime.now()
+			models.storage.new(self)
 
 
 	def __str__(self):
@@ -40,6 +42,7 @@ class BaseModel:
 			updates the public instance attribute updated_at with the current datetime
 		"""
 		self.updated_at = datetime.now()
+		models.storage.save()
 
 	def to_dict(self):
 		"""
@@ -50,4 +53,3 @@ class BaseModel:
 		my_dict["created_at"] = self.created_at.isoformat()
 		my_dict["updated_at"] = self.updated_at.isoformat()
 		return my_dict
-
